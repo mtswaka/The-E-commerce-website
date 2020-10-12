ee355a17c949131ff183cb587e50ceed1b4679d2
mtswaka <32065590+mtswaka@users.noreply.github.com>
2020-10-12 10:23:35 +0200

    Add files via upload

---
+++ body.php
@@ -0,0 +1,150 @@
+
+   <div class="main main-raised">
+  		<div id="hot-deal" class="section mainn mainn-raised">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+						<div class="hot-deal">
+							<a class="primary-btn cta-btn" href="store.php">Shop now</a>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		
+		  
+		
+
+	
+		
+
+		<!-- SECTION -->
+		<div class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+
+					<!-- section title -->
+					<div class="col-md-12">
+						<div class="section-title">
+							<h3 class="title">Products</h3>
+						</div>
+					</div>
+					<!-- /section title -->
+
+					<!-- Products tab & slick -->
+					<div class="col-md-12 mainn mainn-raised">
+						<div class="row">
+							<div class="products-tabs">
+								<!-- tab -->
+								<div id="tab2" class="tab-pane fade in active">
+									<div class="products-slick" data-nav="#slick-nav-2">
+										<!-- product -->
+										<?php
+                    include 'db.php';
+								
+					
+					$product_query = "SELECT * FROM products,categories WHERE product_cat=cat_id  order by RAND()";
+                $run_query = mysqli_query($con,$product_query);
+                if(mysqli_num_rows($run_query) > 0){
+
+                    while($row = mysqli_fetch_array($run_query)){
+                        $pro_id    = $row['product_id'];
+                        $pro_cat   = $row['product_cat'];
+                        $pro_brand = $row['product_brand'];
+                        $pro_title = $row['product_title'];
+                        $pro_price = $row['product_price'];
+                        $pro_image = $row['product_image'];
+
+                        $cat_name = $row["cat_title"];
+
+                        echo "
+				
+                        
+                                
+								<div class='product'>
+									<a href='product.php?p=$pro_id'><div class='product-img'>
+										<img src='product_images/$pro_image' style='max-height: 170px;' alt=''>
+									</div></a>
+									<div class='product-body'>
+										<p class='product-category'>$cat_name</p>
+										<h3 class='product-name header-cart-item-name'><a href='product.php?p=$pro_id'>$pro_title</a></h3>
+										<h4 class='product-price header-cart-item-info'>$pro_price</h4>
+									</div>
+									<div class='add-to-cart'>
+										<button pid='$pro_id' id='product' class='add-to-cart-btn block2-btn-towishlist' href='#'><i class='fa fa-shopping-cart'></i> add to cart</button>
+									</div>
+								</div>
+                               
+							
+                        
+			";
+		}
+        ;
+      
+}
+?>
+										
+										<!-- /product -->
+									</div>
+									<div id="slick-nav-2" class="products-slick-nav"></div>
+								</div>
+								<!-- /tab -->
+							</div>
+						</div>
+					</div>
+					<!-- /Products tab & slick -->
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+<br>
+<br>
+<br>
+		<!-- SECTION -->
+		<div class="section mainn mainn-raised">
+		
+		
+			<!-- container -->
+			<div class="container">
+			
+				<!-- row -->
+				<div class="row">
+					<?php
+					$brand = mysqli_query($con,"SELECT * FROM brands where brand_id in (SELECT product_brand from products ) order by rand() limit 3");
+					while($row=mysqli_fetch_assoc($brand)):
+						$product = mysqli_query($con,"SELECT * FROM products where product_brand = '".$row['brand_id']."' order by rand() limit 1");
+						$prod = mysqli_fetch_array($product);
+					?>
+					<!-- shop -->
+					<div class="col-md-4 col-xs-6">
+						<a href="product.php?p=78"><div class="shop">
+							<div class="shop-img">
+								<img src="product_images/<?php echo $prod['product_image'] ?>" alt="">
+							</div>
+							<div class="shop-body">
+								<h3><?php echo $row['brand_title'] ?></h3>
+								<a href="product.php?p=<?php echo $prod['product_id'] ?>" class="cta-btn">Shop now <i class="fa fa-arrow-circle-right"></i></a>
+							</div>
+						</div></a>
+					</div>
+					<!-- /shop -->
+				<?php endwhile; ?>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
+		<!-- /SECTION -->
+
+		
+</div>
+		
